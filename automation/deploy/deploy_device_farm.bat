3b507d2060350c11f0b6be5acd6256e1c78a1e81
Purushottam Bitle <puru.bitle@gmail.com>
2025-04-25 18:39:15 +0530

    Update deploy_device_farm.bat

--- automation/deploy/deploy_device_farm.bat
+++ automation/deploy/deploy_device_farm.bat
@@ -26,4 +26,20 @@ echo python %APP_DIR%\main.py >> %APP_DIR%\start_device_farm.bat
 echo Creating startup task...
 schtasks /Create /F /TN "DeviceFarmService" /TR "%APP_DIR%\start_device_farm.bat" /SC ONSTART /RL HIGHEST
 
+
+echo @echo off > %APP_DIR%\start_device_farm.bat
+
+:: Set text color to green (0A)
+echo color 0A >> %APP_DIR%\start_device_farm.bat
+
+:: Print banner-style message
+echo echo ########################################################## >> %APP_DIR%\start_device_farm.bat
+echo echo #                                                        # >> %APP_DIR%\start_device_farm.bat
+echo echo #   DO NOT CLOSE THIS WINDOW                              # >> %APP_DIR%\start_device_farm.bat
+echo echo #   Device Farm Microservice is RUNNING on this system    # >> %APP_DIR%\start_device_farm.bat
+echo echo #                                                        # >> %APP_DIR%\start_device_farm.bat
+echo echo ########################################################## >> %APP_DIR%\start_device_farm.bat
+echo echo. >> %APP_DIR%\start_device_farm.bat
+
+
 echo ✅ Device Farm Microservice is ready and will run on system startup.
